8838a3a78cfc3d0b8c3a0ddfe6e928dbc44b8c4d
phamg2807-art <phamg2807@gmail.com>
2026-06-30 20:03:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("SjpWorkspace Engine is active!")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "SjpWorkspace Engine is active!"
+
+if __name__ == "__main__":
+    # Render assigns a port automatically; we must use that
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
